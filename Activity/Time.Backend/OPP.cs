3f596ebd87f9b9ad9cec4cf36e461fa0ac432a75
Brayan Herrera Hernández <brayanherrerahernandez6@gmail.com>
2026-02-24 20:20:45 -0500

    Next Solution

--- Activity/Time.Backend/OPP.cs
+++ Activity/Time.Backend/OPP.cs
@@ -20,67 +20,95 @@ public class OPP
 
     public OPP(int hour)
     {
-        _hour = hour;
+        Hour = hour;
     }
 
     public OPP(int hour, int minute)
     {
-        _hour = hour;
-        _minute = minute;
+        Hour = hour;
+        Minute = minute;
     }
 
     public OPP(int hour, int minute, int second)
     {
-        _hour = hour;
-        _minute = minute;
-        _second = second;
+        Hour = hour;
+        Minute = minute;
+        Second = second;
     }
 
     public OPP(int hour, int minute, int second, int millisecond)
     {
-        _hour = hour;
-        _minute = minute;
-        _second = second;
-        _millisecond = millisecond;
+        Hour = hour;
+        Minute = minute;
+        Second = second;
+        Millisecond = millisecond;
     }
     public int Hour 
     {
         get => _hour;
-        set => _hour = value;
+        set => _hour = ValidHours(value);
 
     }
 
     public int Millisecond 
     {
         get => _millisecond;
-        set => _millisecond = value;
+        set => _millisecond = ValidMilliseconds(value);
     }
 
     public int Minute
     { 
         get => _minute;
-        set => _minute = value;
+        set => _minute = ValidMinute(value);
     }
 
     public int Second
     { 
         get => _second;
-        set => _second = value;
+        set => _second = ValidSecond(value);
     }
 
 
+    public override string ToString()
+    {
+        return $"{Hour:D2}:{Minute:D2}:{Second:D2}:{Millisecond:D}";
+    }
 
+    private int ValidHours(int hour)
+    {
 
+        if (hour < 0 || hour > 23)
+        {
+            throw new ArgumentOutOfRangeException(nameof(hour), "Error: Las horas deben estar entre 0 y 23.");
+        }
+        return hour - 12;
+    }
 
+    private int ValidMilliseconds(int millisecond)
+    {
+        if (millisecond < 0 || millisecond > 999)
+        {
+            throw new ArgumentOutOfRangeException(nameof(millisecond), "Error: Los milisegundos deben estar entre 0 y 999.");
+        }
+        return millisecond;
+    }
 
+    private int ValidMinute(int minute)
+    {
+        if (minute < 0 || minute > 59)
+        { 
+            throw new ArgumentOutOfRangeException(nameof(minute), "Error: Los minutos deben estar entre 0 y 59.");
+        }
+        return minute;
+    }
 
-
-
-
-
-
-
-
-
+    private int ValidSecond(int second)
+    {
+        if (second < 0 || second > 59)
+        {
+            throw new ArgumentOutOfRangeException(nameof(second), "Error: Los segundos deben estar entre 0 y 59.");
+        }
+        return second;
+    }
 
 }
--- Activity/Time.Frontend/Program.cs
+++ Activity/Time.Frontend/Program.cs
@@ -1,3 +1,22 @@
 ﻿using Time.Backend;
 
-var time1 = new OPP();
+try
+{
+    var t1 = new OPP();
+    var t2 = new OPP(14);
+    var t3 = new OPP(9, 34);
+    var t4 = new OPP(19, 45, 56);
+    var t5 = new OPP(25, 3, 45, 678);
+
+    Console.WriteLine(t1);
+    Console.WriteLine(t2);
+    Console.WriteLine(t3);
+    Console.WriteLine(t4);
+    Console.WriteLine(t5);
+
+}
+catch (Exception exception)
+{
+    Console.WriteLine(exception.Message);
+}
+
